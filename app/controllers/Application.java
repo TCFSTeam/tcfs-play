f72bad08074382680af5e89e51c19961369e9c16
a-legotin <mrparovoz@gmail.com>
2014-10-12 22:14:44 +0400

    Login functionality implemented

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import models.User;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 import views.html.login;
+
+import static play.data.Form.form;
 
 public class Application extends Controller {
 
@@ -11,20 +15,36 @@ public class Application extends Controller {
         return ok(index.render("Ok. Hello, motherfucker!"));
     }
 
-    public static Result login() {
-        return ok(
-                login.render()
-        );
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                    routes.Application.index()
+            );
+        }
     }
 
-    public static Result authenticate() {
-        return ok();
+    public static Result login() {
+        return ok(
+                login.render(form(Login.class))
+        );
     }
 
     public static class Login {
 
         public String email;
         public String password;
+
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
 
     }
 }
